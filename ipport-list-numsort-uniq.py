5d8a5ec00ff16f81f42cf9e6f03a93723f307985
hvqzao <hvqzao@gmail.com>
2015-11-10 22:08:34 +0100

    Unix fileformat, masscan added to numsort-uniq

--- ipport-list-numsort-uniq.py
+++ ipport-list-numsort-uniq.py
@@ -20,6 +20,11 @@ unicornscan:
 
 TCP open a.b.c.d:e  ttl 128
 TCP open                     ssh[ port]         from a.b.c.d  ttl 128
+
+masscan (-oL):
+
+open tcp e a.b.c.d 1447188919
+
 """
 
 import sys
@@ -36,6 +41,7 @@ for i in map(lambda x: x.strip(), open(filename).read().strip().split('\n')):
 	i = re.sub(r'^\(UNKNOWN\) \[([0-9\.]+)\] ([0-9]+) \([^\)]*\) open', r'\1 \2', i)
 	i = re.sub(r'^TCP open ([0-9\.]+):([0-9]+)\s+ttl.*$', r'\1 \2', i)
 	i = re.sub(r'^TCP open\s+[^\[]+\[\s*([0-9]+)\]\s*from ([0-9\.]+)\s+ttl.*$', r'\2 \1', i)
+	i = re.sub(r'^open tcp ([0-9]+) ([0-9\.]+) [0-9]+$', r'\2 \1', i)
 	if re.match(r'^([0-9\.]+)\s*([0-9]+)$',i) != None:
 		i = re.sub(r'^([0-9\.]+)\s*([0-9]+)$',r'\1 \2',i)
 		ip,port = i.split(' ')
