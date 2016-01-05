50a9afd8fd238a3d49c458fa3092ffa98f0d2d06
hvqzao <hvqzao@gmail.com>
2016-01-05 22:27:28 +0100

    Script enhancements

--- ping-scan.sh
+++ ping-scan.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-
-ping -c 1 $IP | grep from
+#ping -c 1 $IP | grep from
+if [ "$#" -eq 0 ]
+then
+        echo "Usage: `basename $0` <target1> [target2] ..." >&2
+        echo -e "Example use:\n\tcat targets | xargs ./"`basename $0` >&2
+        exit 1
+fi
+IPs=$@
+for i in $IPs ; do
+        ping -c 1 $i 2>&1 &
+done | grep from | tr -d ':' | awk '{print $4}'
