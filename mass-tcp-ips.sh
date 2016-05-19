7a49316d526d0d974ec7c23163934683e9488e1d
hvqzao <hvqzao@gmail.com>
2016-05-19 21:08:03 +0200

    Added retries option

--- mass-tcp-ips.sh
+++ mass-tcp-ips.sh
@@ -2,20 +2,26 @@
 
 if [ "$#" -lt 2 ]
 then
-	echo "Usage: `basename $0` <pps> <target1> [target2] ..." >&2
+	echo "Usage: `basename $0` <retries> <pps> <target1> [target2] ..." >&2
 	exit 1
 fi
+retries=$1
+shift
 pps=$1
 shift
 IPs=$@
 for i in $IPs ; do
 	if [ -e "paused.conf" ]
 	then
-		echo "paused.conf found. Exiting." >&2
+		echo "paused.conf found. Exiting. Use \"masscan --resume paused.conf\" to resume scan or delete the file." >&2
 		break
 	fi
 	f="${i}_mass_tcp.log"
-	time masscan -p1-65535 -oL "$f" --append-output --rate $pps $i
+	cmd="masscan -p1-65535 -oL $f --retries $retries --append-output --rate $pps $i"
+	echo
+	echo "$cmd"
+	echo
+	time $cmd
 	cat "$f"
 	[ -s "$f" ] || echo "(EMPTY)"
 	echo "$f saved." >&2
--- mass-udp-ips.sh
+++ mass-udp-ips.sh
@@ -2,10 +2,12 @@
 
 if [ "$#" -lt 3 ]
 then
-	echo "Usage: `basename $0` <top-n-ports> <pps> <target1> [target2] ..." >&2
+	echo "Usage: `basename $0` <top-n-ports> <retries> <pps> <target1> [target2] ..." >&2
 	exit 1
 fi
 n=$1
+shift
+retries=$1
 shift
 pps=$1
 shift
@@ -17,11 +19,15 @@ top_ports=`sort -r -k3 /usr/share/nmap/nmap-services | egrep -v "^#" | grep udp 
 for i in $IPs ; do
 	if [ -e "paused.conf" ]
 	then
-		echo "paused.conf found. Exiting." >&2
+		echo "paused.conf found. Exiting. Use \"masscan --resume paused.conf\" to resume scan or delete the file." >&2
 		break
 	fi
 	f="${i}_mass_udp_${n}.log"
-	time masscan -p$top_ports -oL "$f" --append-output --rate $pps $i
+	cmd="masscan -p$top_ports -oL $f --retries $retries --append-output --rate $pps $i"
+	echo
+	echo "$cmd"
+	echo
+	time $cmd
 	cat "$f"
 	[ -s "$f" ] || echo "(EMPTY)"
 	echo "$f saved." >&2
