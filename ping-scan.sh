f98dd7f90d064fe2a80b4c601d53a78a7181377c
hvqzao <hvqzao@gmail.com>
2016-06-11 12:04:35 +0200

    Minor enhancements

--- ping-scan.sh
+++ ping-scan.sh
@@ -2,11 +2,13 @@
 #ping -c 1 $IP | grep from
 if [ "$#" -eq 0 ]
 then
-        echo "Usage: `basename $0` <target1> [target2] ..." >&2
+        echo "Usage: `basename $0` <count> <target1> [target2] ..." >&2
         echo -e "Example use:\n\tcat targets | xargs ./"`basename $0` >&2
         exit 1
 fi
+count=$1
+shift
 IPs=$@
 for i in $IPs ; do
-        ping -c 1 $i 2>&1 &
-done | grep from | tr -d ':' | awk '{print $4}'
+        ping -c $count $i 2>&1 &
+done | grep from | tr -d ':' | awk '{print $4}' | sort -u
