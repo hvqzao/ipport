28f824a3f3f74c1948009b2d32e92c0862ceaf4b
hvqzao <hvqzao@gmail.com>
2017-02-02 09:36:14 +0100

    Simlified mini scanners

--- mini-tcp-custom.sh
+++ mini-tcp-custom.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-if [ "$#" -lt 3 ]
+if [ "$#" -lt 2 ]
 then
-	echo "Usage: `basename $0` <target-ip> <delay> [port1] ..." >&2
+	echo "Usage: `basename $0` <target-ip> <delay> [ports...]" >&2
+	echo "       if no ports will be provided, common ports will be used instead." >&2
 	exit 1
 fi
 IP=$1
 shift
 delay=$1
 shift
-PORTs=$@
+if [ "$#" -gt 0 ]
+then
+	PORTs=$@
+else
+	PORTs="$(`dirname $0`/common-ports.sh)"
+fi
 for i in $PORTs ; do
 	#python nc.py $IP $i 2>&1 &
 	nc -zvnw 2 $IP $i 2>&1 &
--- mini-tcp-ips-delayed.sh
+++ mini-tcp-ips-delayed.sh
@@ -4,15 +4,21 @@ PORTs="$(`dirname $0`/common-ports.sh)"
 
 if [ "$#" -eq 0 ]
 then
-	echo "Usage: `basename $0` <target1> [target2] ..." >&2
+	echo "Usage: `basename $0` <delay-secs> <target1> [target2] ..." >&2
 	exit 1
 fi
+delay=$1
+shift
 IPs=$@
 for i in $IPs ; do
 	f="${i}_mini_tcp.log"
 	for j in $PORTs ; do
 		#python nc.py "$i" "$j" 2>&1 &
 		nc -zvnw 2 "$i" "$j" 2>&1 &
+		if [ "$delay" != "0" ]
+		then
+			sleep "$delay"
+		fi
 	done | sed 's/\(.\)\((UNKNOWN)\)/\1\n\2/gm' | grep open >"$f"
 	cat "$f"
 	[ -s "$f" ] || echo "(EMPTY)"
